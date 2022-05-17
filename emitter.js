85b28c3f81da547e51f3e9600e4328bef07f3735
reetika <reetika.singh05@gmail.com>
2022-05-17 18:03:22 +0530

    added event emitter

---
+++ emitter.js
@@ -0,0 +1,8 @@
+var events = require("events");
+var eventEmitter = new events.EventEmitter();
+
+eventEmitter.on("speak", function (name) {
+  console.log(name, "is speaking");
+});
+
+eventEmitter.emit("speak", "Reetika");
